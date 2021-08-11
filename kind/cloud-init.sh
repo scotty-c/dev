dce20f9b35d848f10515479bee7a445da0a61566
scott coulton <scott.coulton@microsoft.com>
2021-08-11 21:50:59 +1000

    fixing kind

--- kind/cloud-init.sh
+++ kind/cloud-init.sh
@@ -7,29 +7,15 @@ echo "# make..."
 sudo apt-get install -y \
         make
 
-echo "# microk8s..."
-sudo snap install microk8s --classic --channel=1.19
-mkdir -p $HOME/.kube/
-sudo usermod -a -G microk8s $USER
-sudo chown -f -R $USER ~/.kube
-sudo microk8s config > $HOME/.kube/config
+echo "# kubectl..."
+sudo snap install kubectl --classic --channel=1.21
 
 tee -a ~/.bash_aliases <<'EOF'
-function kubectl {
-        sudo microk8s kubectl "$@"
-}
-PATH="$PATH:/usr/local/go/bin:'$HOME'/go/bin"
-
 source <(kubectl completion bash)
 source <(kind completion bash)
 
 EOF
 source ~/.bash_aliases
-
-echo "# lazygit..."
-sudo add-apt-repository --yes ppa:lazygit-team/release
-sudo apt-get update
-sudo apt-get install -y lazygit
 
 echo "# golang..."
 VERSION='1.15.11'
@@ -39,7 +25,6 @@ ARCH='amd64'
 curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
 sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
-
 
 echo "# kind..."
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64

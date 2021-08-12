5f7cef68ab6c92dc74453929df9f3a110c76ef97
scott coulton <scott.coulton@microsoft.com>
2021-08-12 21:08:09 +1000

    adding kata microk8s

---
+++ microk8s-kata/cloud-init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -ex pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "# make..."
+sudo apt-get install -y \
+        make
+
+echo "# microk8s..."
+sudo snap install microk8s --classic --channel=1.19
+mkdir -p $HOME/.kube/
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+sudo microk8s config > $HOME/.kube/config
+microk8s enable kata
+tee -a ~/.bash_aliases <<'EOF'
+function kubectl {
+        sudo microk8s kubectl "$@"
+}
+
+source <(kubectl completion bash)
+
+EOF
+source ~/.bash_aliases
+
+echo "# complete!"

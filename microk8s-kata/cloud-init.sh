8a02ccedec986e8a87140497ec5f10966e45582a
scott coulton <scott.coulton@microsoft.com>
2021-08-12 22:44:30 +1000

    adding dns to cluster

--- microk8s-kata/cloud-init.sh
+++ microk8s-kata/cloud-init.sh
@@ -7,15 +7,7 @@ echo "# make..."
 sudo apt-get install -y \
         make
 
-echo "# microk8s..."
-sudo snap install microk8s --classic --channel=1.22
-mkdir -p $HOME/.kube/
-sudo usermod -a -G microk8s $USER
-sudo chown -f -R $USER ~/.kube
-sudo microk8s config > $HOME/.kube/config
-
-microk8s enable kata
-
+echo "# bash_aliases..."
 tee -a ~/.bash_aliases <<'EOF'
 function kubectl {
         sudo microk8s kubectl "$@"
@@ -25,5 +17,16 @@ source <(kubectl completion bash)
 
 EOF
 source ~/.bash_aliases
+
+echo "# microk8s..."
+sudo snap install microk8s --classic --channel=1.22
+mkdir -p $HOME/.kube/
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+sudo microk8s config > $HOME/.kube/config
+
+microk8s enable dns       
+
+microk8s enable kata
 
 echo "# complete!"

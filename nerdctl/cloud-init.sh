2a76098705b6ec5e4780169c008d166b94e7b515
scott coulton <scott.coulton@microsoft.com>
2021-09-09 21:33:49 +1000

    added nerdctl

--- nerdctl/cloud-init.sh
+++ nerdctl/cloud-init.sh
@@ -19,7 +19,7 @@ echo "# nerdctl..."
 curl -OL https://github.com/containerd/nerdctl/releases/download/v0.11.1/nerdctl-full-0.11.1-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/nerdctl
 sudo tar -C /usr/local/nerdctl -xzf nerdctl-full-0.11.1-linux-amd64.tar.gz
-sudo mkdir /opt/cni/bin/
+sudo mkdir -p /opt/cni/bin/
 sudo ln -s /usr/local/nerdctl/libexec/cni/* /opt/cni/bin/
 
 echo "# rootless ..."

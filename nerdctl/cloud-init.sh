3877ef2d03125892bb39ca2eabd7b3b45502e580
scott coulton <scott.coulton@microsoft.com>
2021-09-09 19:49:35 +1000

    added nerdctl

---
+++ nerdctl/cloud-init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "# make..."
+sudo apt-get install -y \
+        make
+
+echo "# nerdctl..."
+curl -OL https://github.com/containerd/nerdctl/releases/download/v0.11.1/nerdctl-full-0.11.1-linux-arm64.tar.gz
+sudo tar -xzf nerdctl-full-0.11.1-linux-arm64.tar.gz -C /usr/local/bin
+
+
+echo "# complete!"

ebb046191f086155aa8c34f2fec0b76f816fa2f1
scott coulton <scott.coulton@microsoft.com>
2021-10-06 22:10:30 +1100

    added dlx

---
+++ dlx/cloud-init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ex pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "# make..."
+sudo apt-get install -y \
+        make
+tee -a ~/.bash_aliases <<'EOF'
+PATH="$PATH:/usr/local/go/bin:'$HOME'/go/bin"
+EOF
+source ~/.bash_aliases
+
+echo "# golang..."
+VERSION='1.15.11'
+OS='linux'
+ARCH='amd64'
+
+curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
+mkdir $HOME/go
+sudo chown -f -R $USER $HOME/go
+
+echo "# dlx..."
+git clone https://github.com/bketelsen/dlx.git
+cd dlx/bin
+./lxd.sh
+./distrobuilder.sh
+./debootstrap.sh
+./sbuid.sh
+
+echo "# complete!"
